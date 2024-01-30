f91c2a48022085647efa21ce73e5c98d1ab051e1
Donavan White <donavan@naviair.lan>
2024-01-30 09:28:14 -0700

    Working on fluid movement

--- script.js
+++ script.js
@@ -18,101 +18,155 @@ const layout = [
 characters = {
     'valla': {
         'idle': {
-            'n': [
-                'characters/valla/idle_w/1.png',
-                'characters/valla/idle_w/2.png',
-                'characters/valla/idle_w/3.png',
-                'characters/valla/idle_w/4.png',
-                'characters/valla/idle_w/5.png',
-            ],
-            'e': [
-                'characters/valla/idle_s/1.png',
-                'characters/valla/idle_s/2.png',
-                'characters/valla/idle_s/3.png',
-                'characters/valla/idle_s/4.png',
-                'characters/valla/idle_s/5.png',
-            ],
-            's': [
-                'characters/valla/idle_s/1.png',
-                'characters/valla/idle_s/2.png',
-                'characters/valla/idle_s/3.png',
-                'characters/valla/idle_s/4.png',
-                'characters/valla/idle_s/5.png',
-            ],
-            'w': [
-                'characters/valla/idle_w/1.png',
-                'characters/valla/idle_w/2.png',
-                'characters/valla/idle_w/3.png',
-                'characters/valla/idle_w/4.png',
-                'characters/valla/idle_w/5.png',
-            ],
+            'n': {
+				'size': {
+					'x': 56,
+					'y': 66,
+				},
+				'images': [
+					'characters/valla/idle_w/1.png',
+					'characters/valla/idle_w/2.png',
+					'characters/valla/idle_w/3.png',
+					'characters/valla/idle_w/4.png',
+					'characters/valla/idle_w/5.png',
+				],
+			},
+            'e': {
+				'size': {
+					'x': 42,
+					'y': 64,
+				},
+				'images': [
+					'characters/valla/idle_s/1.png',
+					'characters/valla/idle_s/2.png',
+					'characters/valla/idle_s/3.png',
+					'characters/valla/idle_s/4.png',
+					'characters/valla/idle_s/5.png',
+				],
+			},
+            's': {
+				'size': {
+					'x': 42,
+					'y': 64,
+				},
+				'images': [
+					'characters/valla/idle_s/1.png',
+					'characters/valla/idle_s/2.png',
+					'characters/valla/idle_s/3.png',
+					'characters/valla/idle_s/4.png',
+					'characters/valla/idle_s/5.png',
+            	],
+			},
+            'w': {
+				'size': {
+					'x': 56,
+					'y': 66,
+				},
+				'images': [
+					'characters/valla/idle_w/1.png',
+					'characters/valla/idle_w/2.png',
+					'characters/valla/idle_w/3.png',
+					'characters/valla/idle_w/4.png',
+					'characters/valla/idle_w/5.png',
+				],
+			},
         
         },
         "go": {
-            "n": [
-                'characters/valla/go_w/1.png',
-                'characters/valla/go_w/2.png',
-                'characters/valla/go_w/3.png',
-                'characters/valla/go_w/4.png',
-                'characters/valla/go_w/5.png',
-                'characters/valla/go_w/6.png',
-                'characters/valla/go_w/7.png',
-                'characters/valla/go_w/8.png',
-            ],
-            "e": [
-                'characters/valla/go_s/1.png',
-                'characters/valla/go_s/2.png',
-                'characters/valla/go_s/3.png',
-                'characters/valla/go_s/4.png',
-                'characters/valla/go_s/5.png',
-                'characters/valla/go_s/6.png',
-                'characters/valla/go_s/7.png',
-                'characters/valla/go_s/8.png',
-            ],
-            "s": [
-                'characters/valla/go_s/1.png',
-                'characters/valla/go_s/2.png',
-                'characters/valla/go_s/3.png',
-                'characters/valla/go_s/4.png',
-                'characters/valla/go_s/5.png',
-                'characters/valla/go_s/6.png',
-                'characters/valla/go_s/7.png',
-                'characters/valla/go_s/8.png',
-            ],
-            "w": [
-                'characters/valla/go_w/1.png',
-                'characters/valla/go_w/2.png',
-                'characters/valla/go_w/3.png',
-                'characters/valla/go_w/4.png',
-                'characters/valla/go_w/5.png',
-                'characters/valla/go_w/6.png',
-                'characters/valla/go_w/7.png',
-                'characters/valla/go_w/8.png',
-            ],
+            "n": {
+                'size': {
+                    'x': 50,
+                    'y': 64,
+                },
+                'images': [
+                    'characters/valla/go_w/1.png',
+                    'characters/valla/go_w/2.png',
+                    'characters/valla/go_w/3.png',
+                    'characters/valla/go_w/4.png',
+                    'characters/valla/go_w/5.png',
+                    'characters/valla/go_w/6.png',
+                    'characters/valla/go_w/7.png',
+                    'characters/valla/go_w/8.png',
+                ],
+            },
+            "e": {
+                'size': {
+                    'x': 60,
+                    'y': 80,
+                },
+                'images': [
+                    'characters/valla/go_s/1.png',
+                    'characters/valla/go_s/2.png',
+                    'characters/valla/go_s/3.png',
+                    'characters/valla/go_s/4.png',
+                    'characters/valla/go_s/5.png',
+                    'characters/valla/go_s/6.png',
+                    'characters/valla/go_s/7.png',
+                    'characters/valla/go_s/8.png',
+                ],
+            },
+            "s": {
+                'size': {
+                    'x': 60,
+                    'y': 80,
+                },
+                'images': [
+                    'characters/valla/go_s/1.png',
+                    'characters/valla/go_s/2.png',
+                    'characters/valla/go_s/3.png',
+                    'characters/valla/go_s/4.png',
+                    'characters/valla/go_s/5.png',
+                    'characters/valla/go_s/6.png',
+                    'characters/valla/go_s/7.png',
+                    'characters/valla/go_s/8.png',
+                ],
+            },
+            "w": {
+                'size': {
+                    'x': 50,
+                    'y': 64,
+                },
+                'images': [
+                    'characters/valla/go_w/1.png',
+                    'characters/valla/go_w/2.png',
+                    'characters/valla/go_w/3.png',
+                    'characters/valla/go_w/4.png',
+                    'characters/valla/go_w/5.png',
+                    'characters/valla/go_w/6.png',
+                    'characters/valla/go_w/7.png',
+                    'characters/valla/go_w/8.png',
+                ],
+            },
         },
         "death": {
-            "" : [
-                'characters/valla/death/01.png',
-                'characters/valla/death/02.png',
-                'characters/valla/death/03.png',
-                'characters/valla/death/04.png',
-                'characters/valla/death/05.png',
-                'characters/valla/death/06.png',
-                'characters/valla/death/07.png',
-                'characters/valla/death/08.png',
-                'characters/valla/death/09.png',
-                'characters/valla/death/10.png',
-                'characters/valla/death/11.png',
-                'characters/valla/death/12.png',
-                'characters/valla/death/13.png',
-                'characters/valla/death/14.png',
-                'characters/valla/death/15.png',
-                'characters/valla/death/16.png',
-                'characters/valla/death/17.png',
-                'characters/valla/death/18.png',
-                'characters/valla/death/19.png',
-                'characters/valla/death/20.png',
-            ]
+            "" : {
+                'size': {
+                    'x': 70,
+                    'y': 140,
+                },
+                'images': [
+                    'characters/valla/death/01.png',
+                    'characters/valla/death/02.png',
+                    'characters/valla/death/03.png',
+                    'characters/valla/death/04.png',
+                    'characters/valla/death/05.png',
+                    'characters/valla/death/06.png',
+                    'characters/valla/death/07.png',
+                    'characters/valla/death/08.png',
+                    'characters/valla/death/09.png',
+                    'characters/valla/death/10.png',
+                    'characters/valla/death/11.png',
+                    'characters/valla/death/12.png',
+                    'characters/valla/death/13.png',
+                    'characters/valla/death/14.png',
+                    'characters/valla/death/15.png',
+                    'characters/valla/death/16.png',
+                    'characters/valla/death/17.png',
+                    'characters/valla/death/18.png',
+                    'characters/valla/death/19.png',
+                    'characters/valla/death/20.png',
+                ]
+            }
         }
     }
 }
@@ -129,6 +183,10 @@ const character = {
     frameIndex: 0,
     frameRate: 4,
     frameAccumulator: 0,
+    path: [],
+    pathIndex: null,
+    targetX: null,
+    targetY: null,
     animationTimeout: null
 };
 
@@ -178,29 +236,44 @@ function calculatePath(startX, startY, targetX, targetY) {
 function moveToTarget() {
     targetX = character.targetX;
     targetY = character.targetY;
-    
-    const path = calculatePath(character.x, character.y, targetX, targetY);
 
-    if (path.length === 0) {
-        // No valid path found
-        return;
+    if (targetX !== null && targetY !== null) {
+        character.path = calculatePath(character.x, character.y, targetX, targetY);
     }
 
-    let pathIndex = 0;
+    if (character.path.length === 0) {
+        // No valid path found
+        return;
+    } else {
+        character.pathIndex = 0;
+    }
 
-    const nextStep = path[pathIndex];
+    const nextStep = character.path[character.pathIndex];
+    console.log(nextStep);
     if (nextStep) {
-        // Switch character animation to "walk"
-        if (character.animation !== "go") {
-            changeAnimation("go", getDirection(character.x, character.y, targetX, targetY));
+        // Check if the character is facing the correct direction
+        const targetDirection = getDirection(character.x, character.y, targetX, targetY);
+        if (character.direction !== targetDirection) {
+            // Switch character animation to the correct direction first
+            changeAnimation("go", targetDirection);
+            return; // Don't move until animation is complete
         }
+
         character.x = nextStep.x;
         character.y = nextStep.y;
+
         updateCharacterPosition();
-        pathIndex++;
-    } else {
-        // Switch character animation back to "idle" once reached the target
-        changeAnimation("idle", getDirection(character.x, character.y, targetX, targetY));
+        character.pathIndex++;
+
+        // Check if the character has reached the target
+        if (!character.path[character.pathIndex]) {
+            // Character has reached the target, switch to "idle" animation
+            character.pathIndex = null;
+            character.path = [];
+            character.targetX = null;
+            character.targetY = null;
+            changeAnimation("idle", character.direction);
+        }
     }
 }
 
@@ -261,18 +334,14 @@ function updateCharacterPosition() {
 }
 
 function animateCharacter() {
-    const frames = characters.valla[character.animation][character.direction];
+    const frames = characters.valla[character.animation][character.direction].images;
     character.element.src = frames[character.frameIndex % frames.length];
     character.frameIndex++;
 
-    var img = new Image();
-    img.src = character.element.src;
-    img.onload = function(){
-        character.width = img.width;
-        character.height = img.height;
-        character.element.style.width = `${character.width}px`;
-        character.element.style.height = `${character.height*2}px`;
-    }
+	character.width = characters.valla[character.animation][character.direction].size.x;
+	character.height = characters.valla[character.animation][character.direction].size.y;
+	character.element.style.width = `${characters.valla[character.animation][character.direction].size.x}px`;
+	character.element.style.height = `${characters.valla[character.animation][character.direction].size.y*2}px`;
 }
 
 function changeAnimation(animation, direction) {
@@ -325,7 +394,7 @@ function updateFpsCounter(currentTime) {
         currentFps = frameCount;
         frameCount = 0;
         lastFpsUpdateTime = currentTime;
-        // displayFps(); // Display the current FPS
+        //displayFps(); // Display the current FPS
     } else {
         frameCount++;
     }
@@ -357,7 +426,6 @@ grid.addEventListener('click', function(event) {
 
     // Use document.elementFromPoint to get the element at the click position
     const clickedElement = document.elementFromPoint(clickX, clickY);
-    console.log(clickedElement);
 
     // Check if the clicked element is a grid cell
     if (clickedElement && clickedElement.classList.contains('tile')) {
@@ -370,14 +438,38 @@ grid.addEventListener('click', function(event) {
         character.targetX = gridXCoord;
         character.targetY = gridYCoord;
         console.log(`Clicked on grid cell at X: ${gridXCoord}, Y: ${gridYCoord}`);
-        moveToTarget(gridXCoord, gridYCoord);
     }
 });
+
+// Function to preload character frames and set image dimensions
+function preloadCharacterFrames() {
+    const frames = characters.valla[character.animation][character.direction].images;
+    const frameCount = frames.length;
+    const images = [];
+    
+    frames.forEach((framePath, index) => {
+        const image = new Image();
+        image.src = framePath;
+        image.onload = function() {
+            images[index] = image;
+            if (images.length === frameCount) {
+                // All frames are loaded, set dimensions and start the game loop
+                const firstImage = images[0];
+                character.width = firstImage.width;
+                character.height = firstImage.height;
+                character.element.style.width = `${character.width}px`;
+                character.element.style.height = `${character.height * 2}px`;
+                requestAnimationFrame(gameLoop);
+            }
+        };
+    });
+}
 
 // Initialize grid, character, and adjust zoom
 createGrid();
 createCharacter();
 adjustZoom();
+preloadCharacterFrames();
 changeAnimation('idle', 's');
 window.changeAnimation = changeAnimation;
 
